294b11521961876c6e1803118e43624f4b831a9f
Varun Singh <104829215+VarunSingh19@users.noreply.github.com>
2023-12-24 17:25:36 +0530

    Stat Card Fixing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import LOGO from '../../assets/Sitelogo.png';
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font border-t-2">
       <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
         <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <Link to='/'>
+          <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
             <img src={LOGO} style={{width:'45px'}}/>
             <span className="ml-3 text-2xl">Shoppy</span>
-          </a>
+          </div>
+          </Link>
           <p className="mt-2 text-sm text-gray-500">Shoppy is a Ecommerce Website</p>
         </div>
         <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
@@ -16,7 +19,14 @@ const Footer = () => {
             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
             <nav className="list-none mb-10">
               <li>
+                <Link to='/about'>
                 <a className="text-gray-600 hover:text-gray-800">About Us</a>
+                </Link>
+              </li>
+              <li>
+                <Link to='/contact'>
+                <a className="text-gray-600 hover:text-gray-800">Contact Us</a>
+                </Link>
               </li>
               <li>
                 <a className="text-gray-600 hover:text-gray-800">Our Services</a>
--- src/components/StatCard/index.js
+++ src/components/StatCard/index.js
@@ -33,23 +33,29 @@ const Stats = () => {
 
           <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
             <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-[#00B377] w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-                <path d="M8 17l4 4 4-4m-4-5v9"></path>
-                <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 24 24" className="text-[#00B377] mx-auto">
+                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
               </svg>
-              <h2 className="title-font font-medium text-3xl text-gray-900">1M+</h2>
-              <p className="leading-relaxed">Downloads</p>
+              <h2 className="title-font font-medium text-3xl text-gray-900">13+</h2>
+              <p className="leading-relaxed">Store Location</p>
             </div>
           </div>
           <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
             <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-[#00B377] w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-                <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
-                <circle cx="9" cy="7" r="4"></circle>
-                <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
-              </svg>
-              <h2 className="title-font font-medium text-3xl text-gray-900">86K+</h2>
-              <p className="leading-relaxed">Monthly Users</p>
+              <div className='flex'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-paypal" viewBox="0 0 24 24" className="text-[#00B377] mx-auto">
+                  <path d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.351.351 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91.379-.27.712-.603.993-1.005a4.942 4.942 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.687 2.687 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.695.695 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016c.217.124.4.27.548.438.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.873.873 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.352.352 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32.845-5.214Z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-stripe" viewBox="0 0 24 24" className="text-[#00B377] mx-auto">
+                  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.226 5.385c-.584 0-.937.164-.937.593 0 .468.607.674 1.36.93 1.228.415 2.844.963 2.851 2.993C11.5 11.868 9.924 13 7.63 13a7.662 7.662 0 0 1-3.009-.626V9.758c.926.506 2.095.88 3.01.88.617 0 1.058-.165 1.058-.671 0-.518-.658-.755-1.453-1.041C6.026 8.49 4.5 7.94 4.5 6.11 4.5 4.165 5.988 3 8.226 3a7.29 7.29 0 0 1 2.734.505v2.583c-.838-.45-1.896-.703-2.734-.703Z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-credit-card-fill" viewBox="0 0 24 24" className="text-[#00B377] mx-auto">
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1" />
+                </svg>
+              </div>
+              <h2 className="title-font font-medium text-3xl text-gray-900">Supports</h2>
+              <p className="leading-relaxed">Multiple Payment</p>
             </div>
           </div>
         </div>
